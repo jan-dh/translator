d52e3e504522730d93dca69cdd75f48e95bbd3a6
Jan <jan@glue.be>
2019-08-07 11:49:17 +0200

    wip

--- resources/js/src/index.js
+++ resources/js/src/index.js
@@ -1,5 +1,14 @@
 import { Sortable } from '@shopify/draggable';
 
+const divs = document.querySelectorAll('.js-translator .checkbox-group > div');
+
+Array.from(divs).forEach((div)=>{
+  const move = document.createElement("div");
+  move.className = "icon move";
+  div.prepend(move);
+});
+
+
 function SimpleList() {
   const containerSelector = '.js-translator .checkbox-group';
   const containers = document.querySelectorAll(containerSelector);
@@ -8,7 +17,7 @@ function SimpleList() {
   }
   const sortable = new Sortable(containers, {
     draggable: '.checkbox-group > div',
-    handle:'label',
+    handle:'div.move',
     mirror: {
       appendTo: containerSelector,
       constrainDimensions: true,
