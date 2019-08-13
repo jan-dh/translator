e67c2b01f9b68bcc6e6785228a2a89a4b0a63e1b
Jan <jan@glue.be>
2019-08-13 22:51:58 +0200

    Update

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,12 @@ All notable changes to this project will be documented in this file.
 
 The format is based on [Keep a Changelog](http://keepachangelog.com/) and this project adheres to [Semantic Versioning](http://semver.org/).
 
+
+## 1.0.1 - 2019-08-13
+### Added
+- Better instructions
+- Template translations for NL
+
 ## 1.0.0 - 2019-07-28
 ### Added
 - Initial release
---
+++ src/translations/nl/translator.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Translator plugin for Craft CMS 3.x
+ *
+ * A translation field for Craft CMS
+ *
+ * @link      https://www.thebasement.be/
+ * @copyright Copyright (c) 2019 Jan D'Hollander
+ */
+
+/**
+ * @author    Jan D'Hollander
+ * @package   Translator
+ * @since     0.1.0
+ */
+return [
+    'Translator plugin loaded' => 'Translator plugin geladen',
+    'Source' => 'Bron',
+    'Translation' => 'Vertaling',
+    'Select the translations you want to use in this field' => 'Selecteerd de vertalingen die je wil gebruiken in dit veld'
+];
