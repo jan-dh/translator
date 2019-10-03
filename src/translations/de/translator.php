4d29038732c327e6d20b1ebde81b868ff309249a
Simon Müller <simon.d.mueller@gmail.com>
2019-10-03 10:37:03 +0200

    Create translator.php

---
+++ src/translations/de/translator.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Translator plugin for Craft CMS 3.x
+ *
+ * A translation field for Craft CMS
+ *
+ * @link      https://www.thebasement.be/
+ * @copyright Copyright (c) 2019 Jan D'Hollander
+ */
+/**
+ * @author    Jan D'Hollander
+ * @package   Translator
+ * @since     0.1.0
+ */
+return [
+    'Translator plugin loaded' => 'Translator plugin geladen',
+    'Source' => 'Quelle',
+    'Translation' => 'Übersetzung',
+    'Select the translations you want to use in this field' => 'Wähle die Übersetzung, die du für dieses Feld möchtest',
+];
